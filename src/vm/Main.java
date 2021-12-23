4af27271c77fa682ea953a5fd6acbbdabab1e2f4
Xianghao Wang <xianghaowang2001@outlook.com>
2021-12-23 17:59:03 +0800

    update Main: complete the main logic

--- src/vm/Main.java
+++ src/vm/Main.java
@@ -1,8 +1,33 @@
 package vm;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length < 1 || !args[0].endsWith(".vm")) {
+            System.out.println("Usage: java -jar vmtrans.jar filename.vm");
+            return;
+        }
 
+        // convert file name to streams
+        String inFile = args[0];
+        String outFile = inFile.substring(0, inFile.length() - ".vm".length()) + ".asm";
+        FileInputStream in = new FileInputStream(inFile);
+        FileOutputStream out = new FileOutputStream(outFile);
+
+        // translate
+        Parser parser = new Parser(in);
+        CodeWriter coder = new CodeWriter(out);
+        while (parser.hasMoreCommands()) {
+            parser.advance();
+            switch (parser.commandType()) {
+                case C_ARITHMETIC -> coder.writeArithmetic(parser.arg1());
+                case C_PUSH, C_POP -> coder.writePushPop(parser.commandType(), parser.arg1(), parser.arg2());
+            }
+        }
+
+        // close the file
+        coder.close();
     }
 }
